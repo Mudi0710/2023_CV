0d4b94f26b849ca5d486dc97eb842b691dc5900d
Mudi0710 <nomis820710@gmail.com>
2023-03-02 23:33:52 +0800

    修改-js/gsapDefined.js：隱藏 console.log
    隱藏 console.log

--- js/gsapDefined.js
+++ js/gsapDefined.js
@@ -4,7 +4,7 @@ gsap.registerPlugin(ScrollTrigger)
 // 導覽列 Active 位置
 $('.link').each(function (index, link) {
   let href = $(link).attr('href') // 抓 id #home、#about、#skill...
-  console.log(href)
+  // console.log(href)
 
   gsap.to(link, {
     scrollTrigger: {
